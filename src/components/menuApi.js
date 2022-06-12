7f35c550e81ba0055c7dd8cc0ab913369c3cb0b7
Akhilesh kushwah <Akhileshkushwah17@gmail.com>
2022-06-12 11:26:26 +0530

    deployment configuration added

--- src/components/menuApi.js
+++ src/components/menuApi.js
@@ -81,15 +81,6 @@ const Menu = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
   },
-  {
-    id: 10,
-    image: "../images/samosa.jpg",
-    name: "Kachori",
-    category: "kachori",
-    price: "10₹",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-  },
 ];
 
 export default Menu;
